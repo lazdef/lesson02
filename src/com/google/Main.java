e25db9e9af980dbe4d985e14514d6e7bee0e7d76
lazdef <danilzizevsk2005@gmail.com>
2020-08-13 20:04:26 +0300

    строка2 (привет?)

--- src/com/google/Main.java
+++ src/com/google/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello?7777 ");
+        System.out.println("Hello?");
+        System.out.println("Hello?");
     }
 }
